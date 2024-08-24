0c2f5e8e9186f928f0e8207cbf37f72d0f18aa5d
Kwabena Gyampo <dentukwabena.kg@gmail.com>
2024-08-24 14:48:33 +0000

    Changed main function to use argparse, essentially making the run function much smaller.

--- app.py
+++ app.py
@@ -1,101 +1,53 @@
 """Entry point of the application
     """
 
-import sys
-from utils import compare_aligned_sequences, spreadsheet_utils
+from sys import exit
+from utils import compare_aligned_sequences, spreadsheet_utils, arg_parse
 
 
-def run():
+def run(protein_alignments_path, extension, output_file_name) -> None:
     """Run the functions in the order needed based on user
     input
+
+    Args:
+        protein_alignments_path (str): path to the protein alignments file
+        extension (str): extension of the protein alignments file
+        output_file_name (str): name of the output file
+    @return: None
     """
-    print("\t\tWELCOME TO THE ALIGNMENT CLUBHOUSE.\n")
-    print("If you're here, then you want to compare the")
-    print("positions of your isolates to the reference to")
-    print("identify all the mutations, if any, and where they")
-    print("occur.\n\n\n")
+    print("\nCreating new workbook...")
+    workbook, sheet = spreadsheet_utils.create_workbook()
+    print("Workbook created!")
 
-    while True:
-        try:
-            print("These are the options you can choose from:\n")
-            print("\t1.Perform new Comparison.\n")
-            choice = int(input("Enter your choice: "))
+    print("\nExtracting protein names as header columns...")
+    protein_names = spreadsheet_utils.extract_gene_name_from_file(protein_alignments_path)
 
-            if choice == 1:
-                print("\nCreating new workbook...")
-                workbook, sheet = spreadsheet_utils.create_workbook()
-                print("Workbook created!")
+    mutation_finder = (
+            compare_aligned_sequences.MultiFastaMutationsFinder(
+                protein_alignments_path,
+                sheet,
+                protein_names,
+                extension,
+            )
+    )
 
-                print(
-                    "\nProvide the path to the directory containing the protein alignments,"
-                )
-                print(
-                    "after, enter the extension of the alignment files (Default is '.mfa')."
-                )
-                print(
-                    "You then provide the reference sequence ID, after which you enter"
-                )
-                print("The desired name for which you'd like you file to be saved.")
+    print("Processing Multi Fasta Alignment file...")
+    mutation_finder.process_fasta_file()
+    print("Inserting headers and Isolate IDs to excel...")
+    mutation_finder.insert_ids_to_excel()
+    print("Inserting data to excel sheet...")
+    mutation_finder.insert_to_excel()
 
-                protein_alignments_path = input(
-                    "Provide path to the protein alignments directory: "
-                )
-                extension = input("Enter the extension (Default is '.mfa'): ")
-                ref_id = input("Enter your reference sequence ID: ")
-                file_name = input("Output excel file name (DO NOT ADD AN EXTENSION): ")
-
-                print("\nExtracting protein names as header columns...")
-                try:
-                    protein_names = spreadsheet_utils.extract_gene_name_from_file(
-                        protein_alignments_path
-                    )
-                except FileNotFoundError:
-                    print(f"No such file or directory: '{protein_alignments_path}'")
-                    protein_alignments_path = input(
-                        "Kindly enter the right file or directory: "
-                    )
-                    protein_names = spreadsheet_utils.extract_gene_name_from_file(
-                        protein_alignments_path, extension
-                    )
-
-                if extension:
-                    mutation_finder = (
-                        compare_aligned_sequences.MultiFastaMutationsFinder(
-                            protein_alignments_path,
-                            sheet,
-                            ref_id,
-                            protein_names,
-                            extension,
-                        )
-                    )
-                else:
-                    mutation_finder = (
-                        compare_aligned_sequences.MultiFastaMutationsFinder(
-                            protein_alignments_path,
-                            sheet,
-                            ref_id,
-                            protein_names,
-                            ".mfa",
-                        )
-                    )
-
-                print("Processing Multi Fasta Alignment file...")
-                mutation_finder.process_fasta_file()
-                print("Inserting headers and Isolate IDs to excel...")
-                mutation_finder.insert_ids_to_excel()
-                print("Inserting data to excel sheet...")
-                mutation_finder.insert_to_excel()
-
-                # Save to spreadsheet
-                spreadsheet_utils.save_worksheet(workbook, file_name + ".xlsx")
-                print("\nExiting...")
-                sys.exit(0)
-            else:
-                print("\nYour choice is not among the options listed.")
-                print("Enter a choice in the options listed below.\n")
-        except ValueError:
-            print("\nError: Invalid input. Please enter an integer.\n")
+    # Save to spreadsheet
+    spreadsheet_utils.save_worksheet(workbook, output_file_name + ".xlsx")
+    # Save spreadsheet to csv
+    spreadsheet_utils.excel_to_csv(output_file_name + ".xlsx", "Sheet")
+    print("\nExiting...")
+    exit(0)
 
 
 if __name__ == "__main__":
-    run()
+    parser = arg_parse.argparser()
+    args = parser.parse_args()
+
+    run(args.protein_alignment_dir, args.extension, args.output_excel_file)
